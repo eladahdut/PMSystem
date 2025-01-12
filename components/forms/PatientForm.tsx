f3b1a5717221f869bcc75f04fb34f19fd60e8725
eladahdut <eladahdut@gmail.com>
2025-01-12 23:27:17 +0200

    base form to add patient is ready

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,6 +3,19 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+import CustomFormField from '../ui/CustomFormField';
+
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textArea',
+  PHONE_INPUT = 'phoneInput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -11,7 +24,6 @@ const formSchema = z.object({
 });
 
 const PatientForm = () => {
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -25,6 +37,47 @@ const PatientForm = () => {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6 flex-1'>
+        <section className='mb-12 space-y-4'>
+          <h1 className='header'>Hi there 👋</h1>
+          <p className='text-dark-700'>Schedule your first appointment</p>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name='name'
+          label='Full name'
+          placeholder='John Doe'
+          iconSrc='/assets/icons/user.svg'
+          iconAlt='user'
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name='email'
+          label='Email'
+          placeholder='johndoe@strudel.io'
+          iconSrc='/assets/icons/email.svg'
+          iconAlt='email'
+        />
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name='phone'
+          label='Phone'
+          placeholder='+972 XX-XXXXXXX'
+          iconSrc='/assets/icons/phone.svg'
+          iconAlt='phone'
+        />
+
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;
