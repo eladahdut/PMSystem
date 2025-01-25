06800924d3110a0d0a77f83e03c75877b2c8b465
eladahdut <eladahdut@gmail.com>
2025-01-25 17:50:04 +0200

    create user and connection to appwrite

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,9 +3,13 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Button } from '@/components/ui/button';
+import SubmitButton from '@/components/ui/SubmitButton';
 import { Form } from '@/components/ui/form';
 import CustomFormField from '../ui/CustomFormField';
+import { useState } from 'react';
+import { UserFormValidation } from '@/lib/validation';
+import { useRouter } from 'next/navigation';
+import { createUser } from '@/lib/actions/patient.actions';
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -17,25 +21,33 @@ export enum FormFieldType {
   SKELETON = 'skeleton',
 }
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
-});
-
 const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: '',
+      name: '',
+      email: '',
+      phone: '',
     },
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      const userData = { name, email, phone };
+      const user = await createUser(userData);
+      console.log('user', user);
+      if (user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 
   return (
@@ -74,7 +86,7 @@ const PatientForm = () => {
           iconAlt='phone'
         />
 
-        <Button type='submit'>Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
