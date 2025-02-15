282e7b16c2392c571d7754232188928af3e41fab
eladahdut <eladahdut@gmail.com>
2025-02-15 12:54:14 +0200

    created admin page and info cards

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -44,3 +44,44 @@ export const getAppointment = async (appointmentId: string) => {
     console.log('error', error);
   }
 };
+
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [
+        // Query.limit(5),
+        Query.orderDesc('createdAt'),
+      ]
+    );
+    const initialCounts = {
+      scheduledCount: 0,
+      cancelledCount: 0,
+      pendingCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        if (appointment.status === 'scheduled') {
+          acc.scheduledCount += 1;
+        } else if (appointment.status === 'cancelled') {
+          acc.cancelledCount += 1;
+        } else {
+          acc.pendingCount += 1;
+        }
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+    return parseStringify(data);
+  } catch (error) {
+    console.log('error', error);
+  }
+};
